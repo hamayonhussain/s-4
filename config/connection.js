97addad06409c4761ae053a7b34bea0812952083
Hamayon Hussain <hamayonhussain@Hamayons-MacBook-Pro.local>
2018-12-12 22:53:01 -0600

    Dialect changed to mysql in config.json

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,20 @@
-var mysql = require("mysql");
+// var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8080,
-  user: "root",
-  password: null,
-  database: "menu_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 8080,
+//   user: "root",
+//   password: null,
+//   database: "menu_db"
+// });
 
-connection.connect(function(err) {
-  if (err) {
-    // eslint-disable-next-line no-console
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function(err) {
+//   if (err) {
+//     // eslint-disable-next-line no-console
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
-module.exports = connection;
+// module.exports = connection;
